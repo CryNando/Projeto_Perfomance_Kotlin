49fec444aeb2ffbbecf50075323ec035b876aa6e
Luis Fernando <luisfpcf@motorola.com>
2019-11-13 16:05:17 -0300

    Started implementation on API interfaces and repositories

--- app/src/main/java/com/example/projeto_perfomance_kotlin/repositories/IPGeolocationRepository.kt
+++ app/src/main/java/com/example/projeto_perfomance_kotlin/repositories/IPGeolocationRepository.kt
@@ -1,4 +1,22 @@
 package com.example.projeto_perfomance_kotlin.repositories
 
+import com.example.projeto_perfomance_kotlin.apis.IpGeolocationApiInterface
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class IPGeolocationRepository {
+    companion object {
+        const val BASE_URL = "https://ipgeolocation.io/"
+    }
+
+    private val api = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(IpGeolocationApiInterface::class.java)
+
+    fun getNews() {
+        val ip = "200.148.226.100"
+        val call = api.getGeolocation(ip)
+    }
 }
--- app/src/main/java/com/example/projeto_perfomance_kotlin/repositories/NewsApiRepository.kt
+++ app/src/main/java/com/example/projeto_perfomance_kotlin/repositories/NewsApiRepository.kt
@@ -1,4 +1,25 @@
 package com.example.projeto_perfomance_kotlin.repositories
 
+import com.example.projeto_perfomance_kotlin.apis.NewsAPIInterface
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class NewsApiRepository() {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+    }
+
+    private val api = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(NewsAPIInterface::class.java)
+
+    fun getNews() {
+        val q = "bitcoin"
+        val from = "2019-10-13"
+        val sortBy = "publishedAt"
+        val apiKey = "aa3547345b0849ad8deedd045bb18151"
+        val call = api.getNews(q, from, sortBy, apiKey)
+    }
 }
